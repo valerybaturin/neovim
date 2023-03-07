853e34d665bdcb9fc91b53551136e0fa206fe820
Батурин Валерий <vvbaturin@sbercloud.ru>
2023-03-07 12:11:03 +0300

    Update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,27 @@
 return {
-	 -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  }
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("tokyonight").setup({
+				style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	}
 }
-
