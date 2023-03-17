859cd041175c8b3dd7efc2027e69674d583e8229
Батурин Валерий <vvbaturin@sbercloud.ru>
2023-03-17 13:00:48 +0300

    Change theme to catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,27 +1,54 @@
 return {
 	-- the colorscheme should be available when starting Neovim
 	{
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("tokyonight").setup({
-				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				dim_inactive = {
+					enabled = false,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				no_italic = false, -- Force no italic
+				no_bold = false, -- Force no bold
 				styles = {
-					-- Style to be applied to different syntax groups
-					-- Value is any valid attr-list value for `:help nvim_set_hl`
-					comments = { italic = true },
-					keywords = { italic = true },
+					comments = { "italic" },
+					conditionals = {},
+					loops = {},
 					functions = {},
+					keywords = {},
+					strings = {},
 					variables = {},
-					-- Background styles. Can be "dark", "transparent" or "normal"
-					sidebars = "dark", -- style for sidebars, see below
-					floats = "dark", -- style for floating windows
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					notify = false,
+					mini = false,
+					dashboard = true,
+					markdown = true,
+					neotree = true,
+					treesitter = true,
+					treesitter_context = true,
+					lsp_trouble = true,
+					which_key = true,
+					mason = true,
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 				},
 			})
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme catppuccin]])
 		end,
 	}
 }
