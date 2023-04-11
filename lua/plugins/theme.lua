4dd50ef126cd24b114b92f481a451c8f343d950a
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-04-11 13:49:38 +0300

    Change theme to dark plus

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,58 +1,8 @@
 return {
-	-- the colorscheme should be available when starting Neovim
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("catppuccin").setup({
-				flavour = "macchiato", -- latte, frappe, macchiato, mocha
-				dim_inactive = {
-					enabled = true,
-					shade = "dark",
-					percentage = 0.15,
-				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				styles = {
-					comments = { "italic" },
-					conditionals = {},
-					loops = {},
-					functions = {},
-					keywords = { "italic" },
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-				},
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					telescope = true,
-					notify = false,
-					mini = false,
-					dashboard = true,
-					markdown = true,
-					neotree = true,
-					treesitter = true,
-					treesitter_context = true,
-					lsp_trouble = true,
-					which_key = true,
-					mason = true,
-					fidget = true,
-					barbecue = {
-						dim_dirname = true,
-					},
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-				},
-			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	}
+	"martinsione/darkplus.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		vim.cmd [[colorscheme darkplus]]
+	end
 }
