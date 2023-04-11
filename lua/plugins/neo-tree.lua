4856dd483af9b7869e2a8c3c1d8d24107480dc40
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-04-11 20:28:53 +0300

    Update deps

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
 	config = function()
 		require("neo-tree").setup({
 			close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-			popup_border_style = "rounded",
+			popup_border_style = "single",
 			enable_git_status = true,
 			enable_diagnostics = true,
 			sort_case_insensitive = false, -- used when sorting files and directories in the tree
@@ -84,29 +84,29 @@ return {
 					nowait = true,
 				},
 				mappings = {
-						["<space>"] = {
+					["<space>"] = {
 						"toggle_node",
 						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
 					},
-						["<2-LeftMouse>"] = "open",
-						["<cr>"] = "open",
-						["<esc>"] = "revert_preview",
-						["P"] = { "toggle_preview", config = { use_float = true } },
-						["l"] = "focus_preview",
-						["S"] = "open_split",
-						["s"] = "open_vsplit",
+					["<2-LeftMouse>"] = "open",
+					["<cr>"] = "open",
+					["<esc>"] = "revert_preview",
+					["P"] = { "toggle_preview", config = { use_float = true } },
+					["l"] = "focus_preview",
+					["S"] = "open_split",
+					["s"] = "open_vsplit",
 					-- ["S"] = "split_with_window_picker",
 					-- ["s"] = "vsplit_with_window_picker",
-						["t"] = "open_tabnew",
+					["t"] = "open_tabnew",
 					-- ["<cr>"] = "open_drop",
 					-- ["t"] = "open_tab_drop",
-						["w"] = "open_with_window_picker",
+					["w"] = "open_with_window_picker",
 					--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-						["C"] = "close_node",
+					["C"] = "close_node",
 					-- ['C'] = 'close_all_subnodes',
-						["z"] = "close_all_nodes",
+					["z"] = "close_all_nodes",
 					--["Z"] = "expand_all_nodes",
-						["a"] = {
+					["a"] = {
 						"add",
 						-- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
 						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -114,25 +114,25 @@ return {
 							show_path = "none" -- "none", "relative", "absolute"
 						}
 					},
-						["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-						["d"] = "delete",
-						["r"] = "rename",
-						["y"] = "copy_to_clipboard",
-						["x"] = "cut_to_clipboard",
-						["p"] = "paste_from_clipboard",
-						["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+					["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+					["d"] = "delete",
+					["r"] = "rename",
+					["y"] = "copy_to_clipboard",
+					["x"] = "cut_to_clipboard",
+					["p"] = "paste_from_clipboard",
+					["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
 					-- ["c"] = {
 					--  "copy",
 					--  config = {
 					--    show_path = "none" -- "none", "relative", "absolute"
 					--  }
 					--}
-						["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-						["q"] = "close_window",
-						["R"] = "refresh",
-						["?"] = "show_help",
-						["<"] = "prev_source",
-						[">"] = "next_source",
+					["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+					["q"] = "close_window",
+					["R"] = "refresh",
+					["?"] = "show_help",
+					["<"] = "prev_source",
+					[">"] = "next_source",
 				}
 			},
 			nesting_rules = {},
@@ -151,7 +151,7 @@ return {
 						--"*/src/*/tsconfig.json",
 					},
 					always_show = { -- remains visible even if other settings would normally hide it
-						".gitignored",
+						".gitignore",
 					},
 					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
 						--".DS_Store",
@@ -163,7 +163,7 @@ return {
 				},
 				follow_current_file = false, -- This will find and focus the file in the active buffer every
 				-- time the current file is changed while the tree is open.
-				group_empty_dirs = false,    -- when true, empty folders will be grouped together
+				group_empty_dirs = false,   -- when true, empty folders will be grouped together
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
 				-- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -173,17 +173,17 @@ return {
 				-- instead of relying on nvim autocmd events.
 				window = {
 					mappings = {
-							["<bs>"] = "navigate_up",
-							["."] = "set_root",
-							["H"] = "toggle_hidden",
-							["/"] = "fuzzy_finder",
-							["D"] = "fuzzy_finder_directory",
-							["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+						["<bs>"] = "navigate_up",
+						["."] = "set_root",
+						["H"] = "toggle_hidden",
+						["/"] = "fuzzy_finder",
+						["D"] = "fuzzy_finder_directory",
+						["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
 						-- ["D"] = "fuzzy_sorter_directory",
-							["f"] = "filter_on_submit",
-							["<c-x>"] = "clear_filter",
-							["[g"] = "prev_git_modified",
-							["]g"] = "next_git_modified",
+						["f"] = "filter_on_submit",
+						["<c-x>"] = "clear_filter",
+						["[g"] = "prev_git_modified",
+						["]g"] = "next_git_modified",
 					}
 				}
 			},
@@ -194,9 +194,9 @@ return {
 				show_unloaded = true,
 				window = {
 					mappings = {
-							["bd"] = "buffer_delete",
-							["<bs>"] = "navigate_up",
-							["."] = "set_root",
+						["bd"] = "buffer_delete",
+						["<bs>"] = "navigate_up",
+						["."] = "set_root",
 					}
 				},
 			},
@@ -204,13 +204,13 @@ return {
 				window = {
 					position = "float",
 					mappings = {
-							["A"] = "git_add_all",
-							["gu"] = "git_unstage_file",
-							["ga"] = "git_add_file",
-							["gr"] = "git_revert_file",
-							["gc"] = "git_commit",
-							["gp"] = "git_push",
-							["gg"] = "git_commit_and_push",
+						["A"] = "git_add_all",
+						["gu"] = "git_unstage_file",
+						["ga"] = "git_add_file",
+						["gr"] = "git_revert_file",
+						["gc"] = "git_commit",
+						["gp"] = "git_push",
+						["gg"] = "git_commit_and_push",
 					}
 				}
 			}
