184b972666efee50a84584619f209a4d6eec0529
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-04-17 09:59:44 +0300

    Update theme to catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,48 @@
 return {
-	"martinsione/darkplus.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd [[colorscheme darkplus]]
+		require("catppuccin").setup({
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha
+			transparent_background = false,
+			show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+			term_colors = true,
+			dim_inactive = {
+				enabled = false,
+				shade = "dark",
+				percentage = 0.15,
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				telescope = true,
+				notify = false,
+				mini = false,
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
+		})
+		-- setup must be called before loading
+		vim.cmd.colorscheme "catppuccin"
 	end
 }
