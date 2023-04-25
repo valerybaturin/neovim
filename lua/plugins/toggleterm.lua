f82186c48d79a7a5fa974c0144635f22c9b82de1
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2023-04-25 17:56:41 +0300

    Update deps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -20,8 +20,21 @@ return {
 			lazygit:toggle()
 		end
 
+		function _G.set_terminal_keymaps()
+			local opts = { noremap = true }
+			vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+			vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+			vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+			vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+			vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+			vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+		end
+
+		vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 		require("toggleterm").setup {
-			direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float',
+			direction = "vertical", -- 'vertical' | 'horizontal' | 'tab' | 'float',
+			size = 160,
 			float_opts = {
 				-- The border key is *almost* the same as 'nvim_open_win'
 				-- see :h nvim_open_win for details on borders however
